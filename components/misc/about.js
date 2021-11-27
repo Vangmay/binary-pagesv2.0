68a0f50836e3c6f7419c3bd2099a99c381552a1f
Vangmay Sachan <vincetheutuber@gmail.com>
2021-11-27 20:19:59 +0530

    about

--- components/misc/about.js
+++ components/misc/about.js
@@ -1,31 +1,81 @@
+import { useColorModeValue } from '@chakra-ui/color-mode'
 import { Img } from '@chakra-ui/image'
 import { Box, Container, Text } from '@chakra-ui/layout'
+import { textDecoration } from '@chakra-ui/styled-system'
 import React from 'react'
 
 export const About = () => {
+    const textDecor = useColorModeValue('gray.400','cyan.400')
+    const bg= useColorModeValue('orange.500','rgba(23, 25, 35,0.5)')
     return (
-        <div>
-            <Container alignItems='center' textAlign='center' w='100%' bg=''>
-                    <Box w='100%'  display='flex'>
-                        <Box w='100%' minW='100%'>
+        <Container alignItems='center' placeContent='center'  textAlign='center' w='100%' >
+                <Box w='100%' padding='25px' bg={bg} borderRadius='40px'>
+                    <Text w='100%' fontSize='40px'>
+                        Name
+                    </Text>
+                    <Text fontSize='15px'>
+                        (Developer/ Designer/ Learner)
+                    </Text>
+                </Box>
+                <Box w='100%'>
+                    <Box w='100%' minW='100%' paddingTop='12px' >
+                        <Text 
+                            color='purple.300'
+                            paddingBottom='2.5px'
+                          
+                            textAlign='right'
+                            width='100%'
+                            display='flex'
+                            justifyContent='right'
+  
+                        >
+                        
                             <Text 
-                                w='100%' 
-                                textAlign='left'
-                                borderBottom='1.2px solid white'
+                                borderBottom='3px solid '
+                                borderBottomColor={textDecor}
                                 
                             >
                                 About me
-                            </Text>
-                            <Text>I am I am I am Also: I exist</Text>
-                        </Box> 
-                        <Img src='/Images/carcar.gif' w='80%'></Img>
+                            </Text> 
+                        </Text>
+                        <Text 
+                            w='100%'
+                            textAlign='left'
+                            
+                        >I am I am I am Also: I exist</Text>
+                    </Box> 
 
+                </Box>
+                
+                <Box width='100%' >
+                    <Box w='100%' minW='100%' paddingTop='12px' >
+                        <Text 
+                            color='purple.300'
+                            paddingBottom='2.5px'
+                        
+                            textAlign='right'
+                            width='100%'
+                            display='flex'
+                            justifyContent='right'
+                            
+                            >
+                            <Text 
+                            
+                                borderBottom='3px solid '
+                                borderBottomColor={textDecor}
+                            >
+                                I | &lt;3
+                            </Text> 
+                        </Text>
+                        <Text w='100%' textAlign='left'paddingTop='5.5px'> Thing 1, Thing2</Text>
                     </Box>
-                    <Box>
-                        <Text w='100%' textAlign='right'>I | &lt;3 </Text>
-                        <Text w='100%' textAlign='left'> Thing 1, Thing2</Text>
-                    </Box>
-            </Container>
-        </div>
+                </Box>
+                
+               
+
+            
+
+        </Container>
+    
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,10 @@ import '../styles/globals.css'
 import {ChakraProvider} from '@chakra-ui/react'
 import { NewTheme } from '../styles/theme'
 import Layout from '../components/Layouts/Layout'
+import { useColorModeValue } from '@chakra-ui/color-mode'
 
 function MyApp({ Component, pageProps }) {
+  const bg = useColorModeValue('red','blue')
   return (
     <ChakraProvider theme={NewTheme}>
       <Layout>
