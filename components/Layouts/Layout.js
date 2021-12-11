e7a452834930178e13684a3eddc66684953b6f57
Vangmay Sachan <vincetheutuber@gmail.com>
2021-12-11 20:43:42 +0530

    good stuff

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../Navbar'
 import { Image } from '@chakra-ui/image'
 import { Box } from '@chakra-ui/layout'
-import { Container, Spacer } from '@chakra-ui/react'
+import { Container, Spacer, useColorModeValue } from '@chakra-ui/react'
 import { Footer } from '../Footer'
 import { ImageBox } from '../misc/ImageBox'
 import { Animation } from './animation'
 
 
 function Layout({ children }) {
+    const scrollColor = useColorModeValue('red','blue')
     return (
         <Box as='div' h='100vh' w='100vw' transition=''>
             <Navbar/>
@@ -19,7 +20,6 @@ function Layout({ children }) {
                 
             <Footer/>
         </Box>
-
         
 
 
