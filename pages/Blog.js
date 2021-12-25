2610584d54033e62e9461139da259bc76ac14ac3
Vangmay Sachan <vincetheutuber@gmail.com>
2021-12-25 17:47:11 +0530

    Update blog.js

--- pages/Blog.js
+++ pages/Blog.js
@@ -4,49 +4,74 @@ import {
     Grid,
     Container,
     Link,
-
+    
 } from '@chakra-ui/react'
 import axios from 'axios'
 import { BlogMenuItem } from '../components/misc/blogMenuItem.js'
 import ImageBox from '../components/misc/ImageBox.js'
+
 
 function Blog({posts}) {
     // const content = props.posts.data
     // console.log(content[0].attributes.title)
     console.log(posts)
     return (
-        // <>
-        // <ImageBox/>
+        <>
+            <ImageBox/>
 
-        //     <Box
-        //         paddingLeft={12}
-        //         paddingRight={12}
-        //         marginBottom={4.5}
-        //     >
-        //         <Link
-        //         >
-        //             <BlogMenuItem
-        //                 // title={posts[0].title}
-                        
-        //             />
-        //         </Link>
-        //     </Box>
+                <Box
+                    paddingLeft={12}
+                    paddingRight={12}
+                    marginBottom={4.5}
+                    >
+                    <Link
 
-
-
+>
+                        <BlogMenuItem
+                            // title={posts[0].title}
+                            
+                        />
+                    </Link>
+                </Box>
 
 
-        // </>
-        // <h1>{content[0].attributes.title}</h1>    
-        <h1>dshadisj</h1>
+
+
+
+            <h1>dshadisj</h1>
+        </>
     )
-            
+    
 }
 
 export default Blog
 
 export async function getStaticProps(){
     const postsRes = await axios.get('http://localhost:1337/api/posts')
-    const posts = postsRes.data.data
+    const posts = postsRes.data
     return{props : {posts}}
-}
+} 
+
+// export async function getStaticPaths(){
+//     const postsRes = await fetch('http://localhost:1337/api/posts')
+//     const data = await postsRes.json();
+
+//     const paths = data.map(blog => {
+//         return{
+//             params:{id:blog.id.toString()}
+//         }
+//     })
+//     return {
+//         paths,
+//         fallback:false
+//     }
+// }
+
+// export const getStaticProps = async(context)=>{
+//     const id = context.params.id;
+//     const res = await fetch('http://localhost:1337/api/posts/'+id)
+//     const data = await res.json()
+//     return{
+//         props:{blogPost: data}
+//     }
+// }
